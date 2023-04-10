f8aa326aada1116550642474dec4373b327ed425
AlexTraveylan <monsieurdemares@gmail.com>
2023-04-10 17:11:50 +0200

    maj description

--- components/description.tsx
+++ components/description.tsx
@@ -8,15 +8,15 @@ export function Description() {
   }
 
   const description_1: string =
-    "Work-Chrono est une application Next.js de pointage pour le travail conçue pour aider les employés à suivre leur temps de travail de manière précise et efficace. L'application est facile à utiliser et offre une interface utilisateur intuitive pour enregistrer les heures d'entrée et de sortie, les pauses déjeuner, les congés et les heures supplémentaires."
+    'Work-chrono est une application de pointage, permettant de pointer son temps de travail, et ses pauses.'
   const description_2: string =
-    'Avec Work-Chrono, les utilisateurs peuvent créer leur propre compte et accéder à leur tableau de bord personnalisé, qui affiche toutes leurs informations de temps de travail dans un format facile à comprendre. Les employés peuvent facilement visualiser leurs horaires de travail, leurs heures travaillées et leur solde de congé.'
+    "Inutile de créer un compte, connectez-vous avec votre compte google, seulement les informations indispensables sont utilisées par l'application : votre email, et votre nom. Aucune donnée est stockée. Cela vous permet de commencer votre journée à pointer sur un ordinateur et reprendre la session sur votre téléphone ou votre tablette."
   const description_3: string =
-    "L'application prend également en charge plusieurs projets et tâches, permettant aux employés de saisir le temps passé sur chaque projet ou tâche pour une meilleure gestion du temps et une facturation précise des clients."
+    "Avec Work-chrono, vous pouvez créer vos taches ou projets, et ainsi avoir un récapitulatif du temps passé pour chacun d'entre elle pour facturer vos clients précisemment."
   const description_4: string =
-    "Enfin, Work-Chrono est également équipé de fonctionnalités de rapport et d'analyse pour aider les employeurs à suivre et à analyser les données de temps de travail de leurs employés, y compris les heures travaillées, les heures supplémentaires et les absences. Cela permet aux employeurs de prendre des décisions éclairées pour améliorer l'efficacité et la productivité de leur entreprise."
+    'Vous avez accès à vos récapitulatifs pour optimiser votre temps, et optimiser vos habitudes de travail.'
   const description_5: string =
-    'En somme, Work-Chrono est une application Next.js de pointage pour le travail qui offre une solution complète pour suivre et gérer le temps de travail des employés de manière efficace et précise.'
+    'Vous pouvez aussi en fin de mois, télécharger un pdf vos bilans mensuels pour les transmettre a votre employeur.'
   const descriptions: string[] = [
     description_1,
     description_2,
@@ -25,7 +25,7 @@ export function Description() {
     description_5,
   ]
   return (
-    <div className="mx-3">
+    <div className="mx-3 flex flex-col w-[60%] shadow p-3">
       {isDescrHidden ? (
         <h3 className="text-center" onClick={() => toggleDescr()}>
           Voir description
@@ -35,11 +35,13 @@ export function Description() {
           <h3 className="text-center" onClick={() => toggleDescr()}>
             Cacher la description
           </h3>
-          {descriptions.map((desc) => (
-            <p className="my-3" key={desc}>
-              {desc}
-            </p>
-          ))}
+          <div className="flex flex-col items-begin justify-center text-center">
+            {descriptions.map((desc) => (
+              <p className="my-3" key={desc}>
+                {desc}
+              </p>
+            ))}
+          </div>
         </>
       )}
     </div>
